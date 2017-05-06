22f3cbbe0e07fccefaa7dd6481bcff24d156f3a3
angelolovatto <angelolovatto@gmail.com>
2017-05-06 20:37:41 -0300

    Adicionada a mensagem "carregando" durante a requisição ao servidor para evitar que o usuario mexa na interface enquanto o servidor não responda

--- seminario/src/pages/listar_seminarios/listar_seminarios.ts
+++ seminario/src/pages/listar_seminarios/listar_seminarios.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { Servidor } from '../../service/servidor';
 import { DetalhesSeminarioProf } from '../detalhes_seminario_prof/detalhes_seminario_prof';
 import { DetalhesSeminarioAluno } from '../detalhes_seminario_aluno/detalhes_seminario_aluno';
@@ -14,11 +14,17 @@ export class ListarSeminarios {
    private tipo: string;
    private mensagem: string;
    private seminarios: object;  // resposta json
+   loading: any;
 
    constructor(private nav: NavController, private servidor: Servidor,
-               params: NavParams) {
+               params: NavParams, public loadingCtrl: LoadingController) {
       this.nusp = params.get('nusp');
       this.tipo = params.get('tipo');
+
+      this.loading = this.loadingCtrl.create({
+         content: `
+         <ion-spinner >Carregando</ion-spinner>`
+      });
 
       if (this.tipo == 'prof')
          this.mensagem = 'Para confirmações de presença e listagem de alunos, ' +
@@ -30,11 +36,15 @@ export class ListarSeminarios {
    }
 
    listar() {
+      this.loading.present();
+
       this.servidor.get('seminar',
          resp => {
             this.seminarios = resp.data;
+            this.loading.dismiss();
          },
          erro => {
+            this.loading.dismiss();
             alert('ERRO:\n' + erro);
          }
       );
